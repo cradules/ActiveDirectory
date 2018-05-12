8fbe3462ffd8787e6e5a23e010fb0da1c7d873e3
root <root@crypto-vm03-prod-eu-ir.cj.centrys.io>
2018-05-12 19:22:44 +0000

    Update new path and AD user

--- scripts/join.sh
+++ scripts/join.sh
@@ -2,7 +2,7 @@
 
 set timeout 20
 set password [lindex $argv 0]
-spawn /usr/sbin/realm join  -U join_ad@DOMAIN.COM server.com --verbose
+spawn /usr/sbin/realm join  -U join-ad@DOMAIN.COM server.com --verbose
 expect "DOMAIN.COM:"
 send "$password\r";
 interact
